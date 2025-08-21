52f0146dab028f8e997d7e67a8b321a94c5ee35c
rkuouvup <rkuouvup@gmail.com>
2025-08-21 09:31:04 -0600

    Initial Commit

---
+++ cs2370-004-2025f/main.cpp
@@ -0,0 +1,40 @@
+// declare a variable f
+// print variable f
+// declare a variable c
+// convert f to c
+// print variable c
+#include <iostream>
+using namespace std;
+
+// return_type func_name (paras) {}
+/*int main() {
+    //int f;
+    //f = 85;
+    int f;
+    int c = 0;
+    
+    cin >> f;
+    //cout << "The F value is ";
+    //cout << f;
+    //cout << endl;
+    cout << "The F value is " << f << endl;
+    //int c;
+    
+    c = (f - 32) * (5.0 / 9);
+    cout << "The F value is " << c << endl;
+    
+    
+    return 0;
+}*/
+
+int main() {
+    string myString;
+    
+    //cin >> myString;
+    getline(cin, myString);
+    
+    cout << myString << endl;
+    
+    
+    return 0;
+}
