c45a4e4c4b6d4731af7e19aab834218d339b76fe
rkuouvup <rkuouvup@gmail.com>
2025-11-11 16:44:41 -0700

    Nov 11 in-class exercise

--- cs2370-004-2025f/inheritance.cpp
+++ cs2370-004-2025f/inheritance.cpp
@@ -6,13 +6,15 @@
 //
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Business {
 public:
     void SetName(string busName) {name = busName;}
     void SetAddress(string busAddress) {address = busAddress;}
-    string GetDescription() const {return name + "--" + address;}
+    virtual string GetDescription() const {return name + "--" + address;}
+    virtual string GetHours() const = 0;
 protected:
     string name;
     string address;
@@ -22,28 +24,46 @@ class Restaurant : public Business {
 public:
     void SetRating(int userRating) {rating = userRating;}
     int GetRating() const {return rating;}
-    string GetDescription() const {
-        return Business::GetDescription() + ": " + to_string(rating);
+    string GetDescription() const override {
+        return Business::GetDescription() + ": (Rating: " + to_string(rating) + ")";
         
     }
     void DisplayRestaurant() {
         cout << name << "--" << address << ": " << rating << endl;
     }
+    
+    string GetHours() const override {return "open";}
 private:
     int rating;
 };
 
-int main() {
-    Business b1;
+void DriveTo(Business* b) {
+    cout << "Drive to " << b->GetDescription() << endl;
+}
+
+/*int main() {
+    //Business b1;
     Restaurant r1;
-    b1.SetName("my business");
-    b1.SetAddress("my address");
+    //b1.SetName("my business");
+    //b1.SetAddress("my address");
     r1.SetName("my favorite restaurant");
     r1.SetAddress("Orem");
     r1.SetRating(5);
-    cout << b1.GetDescription() << endl;
-    cout << r1.GetDescription() << endl;
+    
+    Business* bptr = &r1;
+    
+    //vector<Business*> bv;
+    //bv.push_back(&b1);
+    //bv.push_back(&r1);
+    
+    //for (auto e : bv)
+    //    cout << e->GetDescription() << endl;
+    
+    //DriveTo(&b1);
+    //DriveTo(&r1);
+    //cout << b1.GetDescription() << endl;
+    //cout << r1.GetDescription() << endl;
     //cout << " Rating: " << r1.GetRating() << endl;
     //r1.DisplayRestaurant();
     return 0;
-}
+}*/
---
+++ cs2370-004-2025f/p5_ex1.cpp
@@ -0,0 +1,54 @@
+//
+//  p5_ex1.cpp
+//  cs2370-004-2025f
+//
+//  Created by Rita Kuo on 11/11/25.
+//
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+class GFG {
+public:
+    float x, y;
+    GFG(float x=0, float y=0) : x(x), y(y) {cout << "constructor: " << x << " " << y << endl;}
+    GFG(const GFG& GFG) :x(GFG.x), y(GFG.y) {cout << "copy: " << x << " " << y << endl;}
+    ~GFG() {cout << "destructor: " << x << " " << y << endl;}
+};
+
+/*int main() {
+    vector<GFG> v;
+    v.reserve(4);
+    
+    int c = 0;
+    for (int i = 0; i < 4; i++) {
+        cout << "========" << i << "========" << endl;
+        //GFG g(c+1, c+2);
+        //v.push_back(g);
+        //v.push_back(GFG(c+1, c+2));
+        v.emplace_back(c+1, c+2);
+        c = c+3;
+    }
+    cout << "======= end of the program ========" << endl;
+    
+    
+    return 0;
+}*/
+
+int main() {
+    //vector<GFG> container;
+    list<GFG> container;
+    
+    int c = 0;
+    for (int i = 0; i < 4; i++) {
+        cout << "========" << i << "========" << endl;
+        container.emplace_back(c+1, c+2);
+        c = c+3;
+    }
+    
+    
+    return 0;
+}
+
+
