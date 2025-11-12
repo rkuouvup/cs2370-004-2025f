f3769d504218e67c6220e341c0477a23c6a82630
rkuouvup <rkuouvup@gmail.com>
2025-11-12 09:55:51 -0700

    Nov 13 given code before the class

---
+++ cs2370-004-2025f/p5_ex2_starter.cpp
@@ -0,0 +1,33 @@
+//
+//  p5_ex2_starter.cpp
+//  cs2370-004-2025f
+//
+//  Created by Rita Kuo on 11/12/25.
+//
+
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+// TODO: Define classes: Badge, Shape (abstract class), Circle, and Rectangle
+
+int main() {
+    //list<Badge> badge_list;
+    vector<vector<string>> info;
+    info.push_back({"Beginner", "Green", "Rectangle"});
+    info.push_back({"Level 10", "Brozen", "Circle"});
+    info.push_back({"Level 50", "Silver", "Circle"});
+    info.push_back({"Professional", "Green", "Rectangle"});
+    info.push_back({"Gatherer", "Orange", "Rectangle"});
+
+    // TODO: Add Badges instances in the badge_list with emplace_back method
+    
+    
+    //for (auto& e : badge_list)
+        // Print the name, color and the size(call area()) of the badge in the PrintInfo() method in the Badge class
+        //e.PrintInfo();
+    
+    return 0;
+}
+
